ebdc0c510034057c1b60c571b6ac8a5370eea200
yaroslav.yuzvak <yaroslav.yuzvak@marconicloud.it>
2025-05-19 09:13:52 +0000

    SItoRegistrazioneModifica

--- main.py
+++ main.py
@@ -4,8 +4,19 @@ from flask import Flask, send_file
 
 app = Flask(__name__)
 
+post= [
+    {"titolo": "Notizia dalla rete: Un nuovo Brawler è in arrivo?" },
+    {"titolo": "Aggiornamento Bilanciamento Brawler: Chi sale e chi scende?"},
+    {"titolo": "Nuova Skin Leggendaria per Leon Disponibile Ora!"},
+]
+
 @app.route("/")
-def index():
+@app.route("/registrazione", methods=["GET"])
+def registrazione():
+    return send_file('src/registrazione.html')
+
+@app.route('/submit_registration', methods=['POST'])
+def submit_registration():
     return send_file('src/index.html')
 
 def main():
